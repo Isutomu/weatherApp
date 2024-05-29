7e6a3e4a0351fdbc4851e80029fd3e296976d7f8
Isutomu <isutomu@gmail.com>
2024-05-29 18:49:04 -0300

    Add units to measures

--- src/weather-widget.js
+++ src/weather-widget.js
@@ -11,11 +11,11 @@ export default function createWeatherVisualizer(
   weatherWidget.appendChild(icon);
 
   const temperature = document.createElement('span');
-  temperature.textContent = weatherData[temperatureUnit];
+  temperature.textContent = `${weatherData[temperatureUnit]} ${temperatureUnit.toUpperCase()}`;
   weatherWidget.appendChild(temperature);
 
   const humidity = document.createElement('span');
-  humidity.textContent = weatherData.hr;
+  humidity.textContent = `${weatherData.hr}%`;
   weatherWidget.appendChild(humidity);
 
   const description = document.createElement('span');
