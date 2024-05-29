eac13b4db38d4227b58405cee588d0ddacf125bd
Isutomu <isutomu@gmail.com>
2024-05-29 13:40:50 -0300

    Make an weather element creator

---
+++ src/weather-widget.js
@@ -0,0 +1,26 @@
+export default function createWeatherVisualizer(
+  weatherData,
+  elemClass,
+  temperatureUnit = 'c',
+) {
+  const weatherWidget = document.createElement('article');
+  weatherWidget.classList.add(elemClass);
+
+  const icon = document.createElement('img');
+  icon.src = weatherData.condition.icon;
+  weatherWidget.appendChild(icon);
+
+  const temperature = document.createElement('span');
+  temperature.textContent = weatherData[temperatureUnit];
+  weatherWidget.appendChild(temperature);
+
+  const humidity = document.createElement('span');
+  humidity.textContent = weatherData.hr;
+  weatherWidget.appendChild(humidity);
+
+  const description = document.createElement('span');
+  description.textContent = weatherData.condition.text;
+  weatherWidget.appendChild(description);
+
+  return weatherWidget;
+}
