0efced6ead052869abf1caea8df00fc3af12f6b9
Isutomu <isutomu@gmail.com>
2024-05-29 20:23:56 -0300

    Adjust to accept unit as a parameter

--- src/populate-forecast.js
+++ src/populate-forecast.js
@@ -2,24 +2,24 @@ import requestWeatherData from './weather-api-handler.js';
 import * as dataProcessor from './weather-data-processor.js';
 import createWeatherVisualizer from './weather-widget.js';
 
-function createFocusedDaily(weatherData) {
+function createFocusedDaily(weatherData, unit) {
   const forecast = dataProcessor.getCurrentWeather(weatherData);
   return createWeatherVisualizer(
     forecast,
     'focused-daily-weather',
     'Today',
-    'c',
+    unit,
   );
 }
 
-function createDaily(weatherData) {
+function createDaily(weatherData, unit) {
   const forecast = dataProcessor.getDailyWeather(weatherData).slice(1);
   return forecast.map((dailyWeather) =>
-    createWeatherVisualizer(dailyWeather, 'daily-weather', 'c'),
+    createWeatherVisualizer(dailyWeather, 'daily-weather', unit),
   );
 }
 
-function createHourly(weatherData, hours) {
+function createHourly(weatherData, hours, unit) {
   const forecast = dataProcessor
     .getRecentHourlyForecast(weatherData, hours)
     .slice(1);
@@ -28,23 +28,23 @@ function createHourly(weatherData, hours) {
       dailyWeather,
       'hourly-weather',
       `${String(dailyWeather.hour).padStart(2, '0')}:00`,
-      'c',
+      unit,
     ),
   );
 }
 
-export default async function populateForecast({ city, days, hours }) {
+export default async function populateForecast({ city, days, hours, unit }) {
   const weatherData = await requestWeatherData(city, days);
 
   const focusedContainer = document.createElement('div');
   focusedContainer.classList.add('weather-focused');
-  focusedContainer.appendChild(createFocusedDaily(weatherData));
+  focusedContainer.appendChild(createFocusedDaily(weatherData, unit));
 
   // container.append(...createDaily(weatherData));
 
   const hourContainer = document.createElement('div');
   hourContainer.classList.add('weather-hourly-container');
-  hourContainer.append(...createHourly(weatherData, hours));
+  hourContainer.append(...createHourly(weatherData, hours, unit));
   focusedContainer.appendChild(hourContainer);
 
   document.querySelector('#weather-container').appendChild(focusedContainer);
