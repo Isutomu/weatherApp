c6f03283d280e42a25bdea75ef10cebb53c9b515
Isutomu <isutomu@gmail.com>
2024-05-29 21:34:07 -0300

    Update dist and change fetch to https

--- src/weather-api-handler.js
+++ src/weather-api-handler.js
@@ -1,5 +1,5 @@
 const requestUrl =
-  'http://api.weatherapi.com/v1/forecast.json?key=9b93bbe006044026a03160236242305';
+  'https://api.weatherapi.com/v1/forecast.json?key=9b93bbe006044026a03160236242305';
 
 export default async function requestWeatherData(city, days = 3) {
   const response = await fetch(`${requestUrl}&q=${city}&days=${days}`, {
