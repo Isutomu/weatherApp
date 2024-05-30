65f7fb9a25d518bfe50b2ba620cca3eddb5c3c08
Isutomu <isutomu@gmail.com>
2024-05-29 21:31:14 -0300

    Update dist with fetch set to cors

--- src/weather-api-handler.js
+++ src/weather-api-handler.js
@@ -2,7 +2,9 @@ const requestUrl =
   'http://api.weatherapi.com/v1/forecast.json?key=9b93bbe006044026a03160236242305';
 
 export default async function requestWeatherData(city, days = 3) {
-  const response = await fetch(`${requestUrl}&q=${city}&days=${days}`);
+  const response = await fetch(`${requestUrl}&q=${city}&days=${days}`, {
+    mode: 'cors',
+  });
   const weatherData = await response.json();
   return weatherData;
 }
